beda91e9392058d2afe2c4d8dfaeaefb49b4392c
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-02-11 00:24:08 -0800

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 class square:
-    """
-    Empty class that defines a square.
-    """
+    """Represent a square."""
     pass
+
+vi 1-square
+
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size
+
