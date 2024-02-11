f6dd46217c97e9ff8762624b448d67e960caf613
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-02-11 00:30:23 -0800

    Task 3

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""Square module."""
+
 class Square:
-    """
-    Class that defines a square.
-    """
+    """Defines a square."""
 
     def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
         self.__size = size
+
