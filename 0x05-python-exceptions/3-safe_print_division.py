2304e701dcaac5c8b33bee7419c622af94883430
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-02-19 05:28:50 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -16,5 +16,5 @@ def safe_print_division(a, b):
     except(ZeroDivisionError):
         div = None
     finally:
-        print("Inside result:{}".format(div))
+        print("Inside result: {}".format(div))
         return (div)
