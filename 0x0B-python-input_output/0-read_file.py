04d8f4796812ab6ea0d4e847ed99f8ad006b1526
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-02-20 00:04:48 -0800

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""a function that reads a text file (UTF8)."""
+
+
+def read_file(filename=""):
+    """prints it to stdout.
+    Args:
+        filename
+    """
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        print(f.read(), end="")
