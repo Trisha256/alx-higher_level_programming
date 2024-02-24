eca3f95672f70e0fa81c76d78e626f76e0f50a07
Patricia Kemirembe <kemirembe2patricia@gmail.com>
2024-02-23 19:34:39 -0800

    Task 1 to 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,6 +95,13 @@ class Rectangle(Base):
             [print(" ", end="") for x in range(self.x)]
             [print("#", end="") for w in range(self.width)]
             print("")
+    
+    def __str__(self):
+        """Returns the print() and str() representation of a circle."""
+        return [Rectangle] ({}) {}/{} - {}/{}.format(self.id, self.x,
+                                                     self.y, self.width,
+                                                     self.height)
+        
 
 
 
